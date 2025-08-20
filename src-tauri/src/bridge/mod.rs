80542b43a9043c322588751256755338b630c6bd
Lorenzo <46894435+Suffro@users.noreply.github.com>
2025-08-20 02:34:51 +0200

    feat: add event bridge commands

--- src-tauri/src/bridge/mod.rs
+++ src-tauri/src/bridge/mod.rs
@@ -3,9 +3,11 @@ pub mod clipboard;
 pub mod files;
 pub mod app;
 pub mod window;
+pub mod events;
 
 pub use notifications::*;
 pub use clipboard::*;
 pub use files::*;
 pub use app::*;
 pub use window::*;
+pub use events::*;
