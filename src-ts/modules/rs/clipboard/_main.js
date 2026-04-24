0edb05746b8c7fa9cd8d6a83828606d7feef15e3
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-04-25 00:05:50 +0200

    dev

--- src-ts/modules/rs/clipboard/_main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildClipboard = buildClipboard;
-function buildClipboard(core) {
-    return {
-        readText: () => core.invoke("dtr_clipboard_read"),
-        writeText: (text) => core.invoke("dtr_clipboard_write", { text }),
-    };
-}
