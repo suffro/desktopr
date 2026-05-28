d74d62c7e2e0bb29a25ce41d48c12f2fb2e1155e
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-05-28 17:03:07 +0200

    dev

--- sdk/dist-sdk/modules/rs/diagnostics/_helpers.js
+++ sdk/dist-sdk/modules/rs/diagnostics/_helpers.js
@@ -5,19 +5,24 @@ exports.buildDiagnosticsTestFunctions = buildDiagnosticsTestFunctions;
 const utils_1 = require("../../../utils");
 const helpers_1 = require("../../../helpers");
 const diagnosticsSettings = async (core, settings) => {
-    if (settings?.retentionDaysAnalytics && !utils_1.Num.isU32(settings.retentionDaysAnalytics))
+    // Accept both camelCase (SDK input) and snake_case (Rust return format)
+    const s = settings;
+    const retDaysAnalytics = s?.retentionDaysAnalytics ?? s?.retention_days_analytics;
+    const retDaysLogs = s?.retentionDaysLogs ?? s?.retention_days_logs;
+    const retDaysCrashes = s?.retentionDaysCrashes ?? s?.retention_days_crashes;
+    if (retDaysAnalytics != null && !utils_1.Num.isU32(retDaysAnalytics))
         throw ("[retentionDaysAnalytics] the value must be a U32 integer number");
-    if (settings?.retentionDaysLogs && !utils_1.Num.isU32(settings.retentionDaysLogs))
+    if (retDaysLogs != null && !utils_1.Num.isU32(retDaysLogs))
         throw ("[retentionDaysLogs] the value must be a U32 integer number");
-    if (settings?.retentionDaysCrashes && !utils_1.Num.isU32(settings.retentionDaysCrashes))
+    if (retDaysCrashes != null && !utils_1.Num.isU32(retDaysCrashes))
         throw ("[retentionDaysCrashes] the value must be a U32 integer number");
     return await core.invoke("dtr_logs_set_privacy", {
         patch: {
-            analytics_enabled: settings?.analyticsEnabled,
-            crash_reports_enabled: settings?.crashReportsEnabled,
-            retention_days_analytics: settings?.retentionDaysAnalytics,
-            retention_days_logs: settings?.retentionDaysLogs,
-            retention_days_crashes: settings?.retentionDaysCrashes,
+            analytics_enabled: s?.analyticsEnabled ?? s?.analytics_enabled,
+            crash_reports_enabled: s?.crashReportsEnabled ?? s?.crash_reports_enabled,
+            retention_days_analytics: retDaysAnalytics,
+            retention_days_logs: retDaysLogs,
+            retention_days_crashes: retDaysCrashes,
         }
     });
 };
--- src-ts/modules/rs/diagnostics/_helpers.ts
+++ src-ts/modules/rs/diagnostics/_helpers.ts
@@ -5,17 +5,23 @@ import { Num } from "../../../utils";
 import { getAppVersion } from "../../../helpers";
 
 export const diagnosticsSettings = async (core: { invoke: DesktoprAPI["invoke"] }, settings?: PrivacySettings): Promise<PrivacySettingsCamelCase> => {
-  if(settings?.retentionDaysAnalytics && !Num.isU32(settings.retentionDaysAnalytics)) throw("[retentionDaysAnalytics] the value must be a U32 integer number");
-  if(settings?.retentionDaysLogs && !Num.isU32(settings.retentionDaysLogs)) throw("[retentionDaysLogs] the value must be a U32 integer number");
-  if(settings?.retentionDaysCrashes && !Num.isU32(settings.retentionDaysCrashes)) throw("[retentionDaysCrashes] the value must be a U32 integer number");
+  // Accept both camelCase (SDK input) and snake_case (Rust return format)
+  const s = settings as any;
+  const retDaysAnalytics = s?.retentionDaysAnalytics ?? s?.retention_days_analytics;
+  const retDaysLogs      = s?.retentionDaysLogs      ?? s?.retention_days_logs;
+  const retDaysCrashes   = s?.retentionDaysCrashes   ?? s?.retention_days_crashes;
+
+  if(retDaysAnalytics != null && !Num.isU32(retDaysAnalytics)) throw("[retentionDaysAnalytics] the value must be a U32 integer number");
+  if(retDaysLogs      != null && !Num.isU32(retDaysLogs))      throw("[retentionDaysLogs] the value must be a U32 integer number");
+  if(retDaysCrashes   != null && !Num.isU32(retDaysCrashes))   throw("[retentionDaysCrashes] the value must be a U32 integer number");
 
   return await core.invoke("dtr_logs_set_privacy", {
     patch: {
-      analytics_enabled: settings?.analyticsEnabled,
-      crash_reports_enabled: settings?.crashReportsEnabled,
-      retention_days_analytics: settings?.retentionDaysAnalytics,
-      retention_days_logs: settings?.retentionDaysLogs,
-      retention_days_crashes: settings?.retentionDaysCrashes,
+      analytics_enabled:        s?.analyticsEnabled     ?? s?.analytics_enabled,
+      crash_reports_enabled:    s?.crashReportsEnabled  ?? s?.crash_reports_enabled,
+      retention_days_analytics: retDaysAnalytics,
+      retention_days_logs:      retDaysLogs,
+      retention_days_crashes:   retDaysCrashes,
     }
   });
 }
